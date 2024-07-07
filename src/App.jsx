028afa328172f886b60b8fdb4f82004ee1f5fce7
Forson Bentum <forsonbentum55@email.com>
2024-07-07 12:57:29 -0700

    added some files and updated the old ones

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import HomeCards from "./components/HomeCards"
 import JobListings from "./components/JobListings"
+import View from "./components/View"
 
 const App = () => {
   return (
@@ -10,21 +11,20 @@ const App = () => {
     <Hero/>
     <HomeCards/>
     <JobListings/>
-   
+    <View/>
+    
+    
+
 
    
+
+   
     
 
     
     
 
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+   
 
     </>
   )
