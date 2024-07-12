3947fb1bb0f8c1840219d8ad1a74b82d8d8aaed2
kofidhope <forsonbentum55@gmail.com>
2024-07-11 21:36:13 -0700

    new commit

--- src/Pages/JobPage.jsx
+++ src/Pages/JobPage.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import JobListing from "../components/JobListing"
+import JobListings from "../components/JobListings"
 
 const JobPage = () => {
   return (
-    <div>
-      JobPage
-    </div>
+    <section className="bg-blue-50 px-4 py-6">
+      <JobListings />
+    </section>
   )
 }
 
